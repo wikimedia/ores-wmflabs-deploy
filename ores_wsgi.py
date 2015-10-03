235f2cd96f11806dd48b3703de6dacab9d49608b
halfak <aaron.halfaker@gmail.com>
2015-10-03 14:16:59 -0500

    Adds logging config to ores_wsgi.py

--- ores_wsgi.py
+++ ores_wsgi.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
 import logging
-import os
-
-from flask import request
 
 import yamlconf
+
 from ores.wsgi import server
 
 config = yamlconf.load(open("ores.wmflabs.org.yaml"))
 
 if 'data_paths' in config['ores'] and \
-    'nltk' in config['ores']['data_paths']:
+   'nltk' in config['ores']['data_paths']:
     import nltk
     nltk.data.path.append(config['ores']['data_paths']['nltk'])
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s:%(name)s -- %(message)s'
+)
 
 application = server.configure(config)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s %(levelname)s:%(name)s -- %(message)s'
-    )
+    logging.setLevel(logging.INFO)
     logging.getLogger('ores.metrics_collectors').setLevel(logging.DEBUG)
 
     application.debug = True
