532509839298925363a268ac970b856f8a67189e
amir <ladsgroup@gmail.com>
2016-03-21 16:22:28 +0000

    Re-work scap3 commands

--- scap/cmd.sh
+++ scap/cmd.sh
@@ -1,6 +1,8 @@
-mkdir -p /srv/ores/venv
-virtualenv --python python3 --system-site-packages /srv/ores/venv
-/srv/ores/venv/bin/pip install --use-wheel --no-deps /srv/ores/ores-wikimedia-config/submodules/wheels/*.whl
+venv="/srv/ores/venv"
+mkdir -p $venv
+virtualenv --python python3 --system-site-packages $venv
+$venv/bin/pip freeze | xargs $venv/bin/pip uninstall -y
+$venv/bin/pip install --use-wheel --no-deps /srv/ores/ores-wikimedia-config/submodules/wheels/*.whl
 ln -s /srv/ores/ores-wikimedia-config /srv/ores/config
 sudo service celery-ores-worker restart
 sudo service flower-ores restart
