6a19215e1b5a42d636715346796a15fbc75da772
halfak <aaron.halfaker@gmail.com>
2016-06-23 14:26:12 -0500

    Removes scap.

--- scap/cmd_web.sh
+++
@@ -1,7 +0,0 @@
-venv="/srv/ores/venv"
-deploy_dir="/srv/deployment/ores/deploy"
-mkdir -p $venv
-virtualenv --python python3 --system-site-packages $venv
-$venv/bin/pip freeze | xargs $venv/bin/pip uninstall -y
-$venv/bin/pip install --use-wheel --no-deps $deploy_dir/submodules/wheels/*.whl
-sudo service uwsgi-ores restart
