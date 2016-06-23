ee53e9147fe518dc903a5930b8e2ea58196d0362
halfak <aaron.halfaker@gmail.com>
2016-06-23 14:25:51 -0500

    uses ores.applications in ores_*.py

--- ores_wsgi.py
+++ ores_wsgi.py
@@ -1,26 +1,7 @@
 #!/usr/bin/env python3
-import glob
-import logging
-import logging.config
+from ores.applications import wsgi
 
-import yamlconf
-from ores.wsgi import server
-
-config_paths = sorted(glob.glob("config/*.yaml") +
-                      glob.glob("/etc/ores/*.yaml"))
-
-config = yamlconf.load(*(open(p) for p in config_paths))
-
-with open("logging_config.yaml") as f:
-    logging_config = yamlconf.load(f)
-    logging.config.dictConfig(logging_config)
-
-if 'data_paths' in config['ores'] and \
-   'nltk' in config['ores']['data_paths']:
-    import nltk
-    nltk.data.path.append(config['ores']['data_paths']['nltk'])
-
-application = server.configure(config)
+application = wsgi.build()
 
 if __name__ == '__main__':
     logging.getLogger('ores').setLevel(logging.DEBUG)
