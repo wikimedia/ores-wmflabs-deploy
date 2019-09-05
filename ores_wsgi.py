789ea457e04ec99316be34daafd9b06e219489a7
halfak <aaron.halfaker@gmail.com>
2019-09-05 14:21:05 -0500

    Splits config and bumps all submodules to HEAD

--- ores_wsgi.py
+++ ores_wsgi.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     logging.getLogger('ores').setLevel(logging.DEBUG)
 
     application.debug = True
-    application.run(host="0.0.0.0", processes=64, debug=True)
+    application.run(host="0.0.0.0", processes=4, threaded=False, debug=True)
